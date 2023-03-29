85dbf8d089f009c3d9da96754887427045d370a2
shahzayb <zeb00002@algonquinlive.com>
2023-03-29 04:38:16 -0400

    small movies app using OMDb API done

---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+// we doing object destructiring in the props so we dont have to repeat props.movie1.... eerytime
+
+const MovieCard = ({movie}) => {
+  return (
+    <div className='movie'>
+                <div>
+                    <p>{movie.Year}</p>
+                </div>
+                <div>
+                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'}  />
+                </div>
+                <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                </div>
+            </div>
+  )
+}
+
+export default MovieCard
